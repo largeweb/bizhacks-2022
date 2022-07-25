341be1a8a73d2ad5f2c3cf041e3c5fc617951369
matt <mat@unc.edu>
2022-07-25 09:54:54 -0400

    committed from script, no message specified

--- mem-pwa/src/pages/ChildCarePage.js
+++ mem-pwa/src/pages/ChildCarePage.js
@@ -7,12 +7,16 @@ import {Link} from "react-router-dom";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Navbar from "./Navbar"
 import Footer from "./Footer"
+import child1 from "../images/child1.jpeg"
 
 function CubiclePage() {
  return (
   <div className='homediv'>
       <Navbar />
-	  <p>Information about the Child Care Page</p>
+      <div className='childcareTopDiv'>
+        <p>Decide if Infosys Child Care is Right for You</p>
+      </div>
+      <img className='childcareimage' src={child1}></img>
 	  <Link to="/"><button className='menuButton'>Go Back Home</button></Link>
       <Footer />
   </div>
